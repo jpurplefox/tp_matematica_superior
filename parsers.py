201093e027049d8675c9d0ff83ade56ad79fce1e
jcollado <collado.juanmaria@gmail.com>
2018-05-25 15:24:04 -0300

    Se hace refactor del código y se agregan test unitarios

---
+++ parsers.py
@@ -0,0 +1,35 @@
+import argparse
+
+def get_parser():
+    """Prepara el parser para interpretar las opciones pasadas por linea de comandos"""
+    parser = argparse.ArgumentParser()
+    add_methods(parser)
+
+    return parser
+
+def add_methods(parser):
+    """Agrega al parser los distintos métodos de resolucion para encontrar la raíz de la funcion"""
+    methods = parser.add_subparsers(
+        title='metodo',
+        dest='method',
+        description='Metodo a utilizar para obtener la raiz de la funcion'
+    )
+    add_bisection_method(methods)
+    add_fixed_point_method(methods)
+    add_newton_raphson_method(methods)
+
+def add_bisection_method(methods):
+    """Agrega el método de biseccion al parser"""
+    bisection_parser = methods.add_parser('biseccion')
+    bisection_parser.add_argument('a', type=int, help='Primer punto del intervalo a aplicar el metodo')
+    bisection_parser.add_argument('b', type=int, help='Segundo punto del intervalo a aplicar el metodo')
+
+def add_fixed_point_method(methods):
+    """Agrega el método de punto fijo al parser"""
+    fixed_point_parser = methods.add_parser('punto-fijo')
+    fixed_point_parser.add_argument('inicial', type=int, help='Valor inicial a usar al aplicar el metodo')
+
+def add_newton_raphson_method(methods):
+    """Agrega el método de Newton Raphson al parser"""
+    newton_raphson_parser = methods.add_parser('newton-raphson')
+    newton_raphson_parser.add_argument('inicial', type=int, help='Valor inicial a usar al aplicar el metodo')
---
+++ test/test_parsers.py
@@ -0,0 +1,24 @@
+import unittest
+
+from parsers import get_parser
+ 
+class TestUM(unittest.TestCase):
+ 
+    def setUp(self):
+        self.parser = get_parser()
+ 
+    def test_bisection_parser(self):
+        args = self.parser.parse_args(['biseccion', '1', '2'])
+        self.assertEqual(args.method, 'biseccion')
+        self.assertEqual(args.a, 1)
+        self.assertEqual(args.b, 2)
+ 
+    def test_fixed_point_parser(self):
+        args = self.parser.parse_args(['punto-fijo', '1'])
+        self.assertEqual(args.method, 'punto-fijo')
+        self.assertEqual(args.inicial, 1)
+ 
+    def test_newton_raphson_parser(self):
+        args = self.parser.parse_args(['newton-raphson', '1'])
+        self.assertEqual(args.method, 'newton-raphson')
+        self.assertEqual(args.inicial, 1)
