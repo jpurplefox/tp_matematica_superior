43911ae7cdb8e1be55ac9e9b9b2b0b82c9d23d80
jcollado <collado.juanmaria@gmail.com>
2018-05-25 22:57:39 -0300

    Se agrega el methodo de bolzano para resolver la funcion x**2 - 2

---
+++ methods.py
@@ -0,0 +1,38 @@
+class BolzanoException(Exception):
+    pass
+
+class Bisection:
+    def __init__(self, func, valor_minimo, valor_maximo):
+        self.func = func
+        self.valor_minimo = valor_minimo
+        self.valor_maximo = valor_maximo
+        self.bolzano_check(self.valor_minimo, self.valor_maximo)
+        self.valor_anterior = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.valor_anterior is None:
+            if self.get_error(self.valor_anterior) <= 0.00001:
+                raise StopIteration()
+
+            try:
+                self.bolzano_check(self.valor_anterior, self.valor_maximo)
+                self.valor_minimo = self.valor_anterior
+            except BolzanoException:
+                self.valor_maximo = self.valor_anterior
+
+        self.valor_anterior = (self.valor_minimo + self.valor_maximo) / 2
+        return self.valor_anterior
+
+    def bolzano_check(self, valor_minimo, valor_maximo):
+        if self.func(valor_minimo) < 0 and self.func(valor_maximo) < 0:
+            raise BolzanoException('El intervalo elegido no cumple Bolzano')
+
+        if self.func(valor_minimo) > 0 and self.func(valor_maximo) > 0:
+            raise BolzanoException('El intervalo elegido no cumple Bolzano')
+
+    def get_error(self, valor):
+        return abs(self.func(valor))
+        
--- resolver.py
+++ resolver.py
@@ -1,3 +1,4 @@
+from methods import Bisection
 from parsers import get_parser
 
 if __name__ == '__main__':
@@ -5,7 +6,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.method == 'biseccion':
-        print(f'Resolviendo por biseccion con intervalo [{args.a},{args.b}]')
+        bisection = Bisection(lambda x: x**2 - 2, args.a, args.b)
+        for i, valor in enumerate(bisection, 1):
+            pass
+        print(f'Se obtuvo el valor {valor} en {i} iteraciones')
     if args.method == 'punto-fijo':
         print(f'Resolviendo por punto fijo con valor inicial {args.inicial}')
     if args.method == 'newton-raphson':
